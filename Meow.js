7ea7d2cc9f158213e6d6ddbc1adcfa4484ae0669
SenkiDaKitty <44231818+SenkiDaKitty@users.noreply.github.com>
2018-10-23 10:39:08 +0200

    Update Meow.js

--- Meow.js
+++ Meow.js
@@ -8,6 +8,7 @@ var idAdmins = ['183549541470044161','472812198654246923'];
 var ProzPrefix = "P";
 var Turtle = "Turtle"
 var Pub = "C"
+var ping = bot.ping
  
 function userPermission(member, perm) {
     if (idAdmins.includes(member.user.id)) {
@@ -127,11 +128,21 @@ bot.on('message', async message => {
         }
     }
     else if (message.content.startsWith(`${botsettings.prefix}ping`)) {
+       if(ping <= 99) {
         var embed = new Discord.RichEmbed()
         .setAuthor("Asked By, Demandé Par : " + message.author.username,message.author.avatarURL)
         .setTitle("<a:blobdance:483297638356353027> Kitty MS <a:blobdance:483297638356353027>")
-        .addField(`${Math.round(bot.ping)}` + "ms <a:blobdance:483297638356353027>",`It's My ping, it's slow sorry, ceci est ma latence c'est assez lent désolé :c`)
-        .setColor("0xff00ff")
+        .addField(`${Math.round(bot.ping)}` + "ms <a:blobdance:483297638356353027>",`Le bot ne lag actuellement pas. Hébergeur : Heroku`)
+        .setColor("0x02e427")
+        .setFooter("©SenkiTheKitty 2018","https://media.discordapp.net/attachments/434459534514454528/481879724910444556/4ef67948850f942f08d52125928feeb5.gif?width=473&height=473")
+        message.delete()
+        message.channel.sendEmbed(embed);
+            } else if(ping <= 200) {
+             var embed = new Discord.RichEmbed()
+        .setAuthor("Asked By, Demandé Par : " + message.author.username,message.author.avatarURL)
+        .setTitle("<a:blobdance:483297638356353027> Kitty MS <a:blobdance:483297638356353027>")
+        .addField(`${Math.round(bot.ping)}` + "ms <a:blobdance:483297638356353027>",`Le bot rencontre quelques problèmes techniques, Sorry :c. Hébergeur : Heroku`)
+        .setColor("0xff0000")
         .setFooter("©SenkiTheKitty 2018","https://media.discordapp.net/attachments/434459534514454528/481879724910444556/4ef67948850f942f08d52125928feeb5.gif?width=473&height=473")
         message.delete()
         message.channel.sendEmbed(embed);
